9857361cfe9baaaf43c4b99b3b9f8e3e23a98870
yanuyanu <yanuar@infoflow.co.id>
2020-02-08 19:15:11 +0700

    fixing favorite screen

--- lib/pages/favorite/favorite_video_detail_page.dart
+++ lib/pages/favorite/favorite_video_detail_page.dart
@@ -7,10 +7,12 @@ class FavoriteDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text('Movie Editor'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.save), onPressed: () {
-
-          }),
-          SizedBox(width: 50,),
+          IconButton(
+            icon: Icon(Icons.save), 
+            onPressed: () {}),
+          SizedBox(
+            width: 30,
+          ),
         ],
       ),
       body: FavoriteDetailPage(),
@@ -30,6 +32,94 @@ class _FavoriteDetailState extends State<FavoriteDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image(
+                        image: NetworkImage(
+                            'https://m.media-amazon.com/images/M/MV5BMTg1NjQwNTI3N15BMl5BanBnXkFtZTcwNDIyNTY1Mw@@._V1_SX300.jpg'),
+                      ),ListTile(
+                        leading: Icon(Icons.movie),
+                        title: Text('Rak haeng Siam'),
+                        subtitle: Text('2007'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      hintText: 'Label',
+                    ),
+                  ),
+              ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      hintText: 'Priority',
+                    ),
+                  ),
+                ),
+                SwitchListTile(
+                  title: Text('Viewed'),
+                  value: false, 
+                onChanged: (bool value){
+
+                }),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      hintText: 'Rating',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      hintText: 'Modified Date',
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
